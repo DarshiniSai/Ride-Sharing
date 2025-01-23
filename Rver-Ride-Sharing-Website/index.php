cc866a525bb09e827a7e6c4fb6d68ea18d836ee6
Darshini Sai Mathi <159278720+DarshiniSai@users.noreply.github.com>
2025-01-23 20:04:05 +0530

    Add files via upload

---
+++ Rver-Ride-Sharing-Website/index.php
@@ -0,0 +1,182 @@
+<?php include_once "header.php" ?>
+
+
+<!-- Banner Part Start -->
+<div id="banner" class="text-white">
+    <div class="banner_overlay">
+        <div class="container">
+            <div class="banner_content">
+                <h1>Book taxis with us</h1>
+                <p>We help you to have a nice and safe journey.
+                </p>
+                <ul class="d-flex justify-content-start">
+                    <li><a href="Chat App/index.php">Ride</a></li>
+                    <li><a href="Chat App/index.php">Earn</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Banner Part Ends-->
+
+<!-- Promo Part Starts-->
+<div id="promo">
+    <div class="container">
+        <div class="section_title">
+            <h2>Convenient, Safe</h2>
+            <p>We ensure the safest journey in the town, anywhere, everywhere</p>
+        </div>
+        <div class="d-flex">
+            <div class="col-lg-6">
+                <div class="promo_content">
+                    <img src="images/promo_pic1.jpg" alt="">
+                    <h3>Convenient</h3>
+                    <p>Taxis are everywhere and it is one of the main transportation system which
+                        we all use regularly. It's small form factor opens up more exploration.</p>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="promo_content">
+                    <img src="images/promo_pic2.jpg" alt="">
+                    <h3>Safe</h3>
+                    <p>Our drivers are well trained to ensure safe journey.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Promo Part Starts-->
+
+
+
+<!-- About part Starts -->
+
+<div id="about">
+    <div class="container">
+        <div class="row">
+            <div class="section_title">
+                <h2>About Us</h2>
+                <p>Learn more about us.</p>
+            </div>
+            <div class="col-lg-6 about_des">
+                <h4>Title</h4>
+                <p>Welcome to Ride-Sharer, a platform designed to revolutionize the way you travel. We aim to connect people, places, and communities by offering an affordable, convenient, and environmentally friendly alternative to traditional transportation.
+                </p>
+                <a class="btn-lg btn-info" href="#" role="button">Learn More</a>
+            </div>
+            <div class="col-lg-6">
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="images/fleet_pic1.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block carousel-overlay">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/fleet_pic5.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block carousel-overlay">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/fleet_pic4.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block carousel-overlay">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- About part Ends -->
+
+<!-- Features Part Starts-->
+
+<div id="features">
+    <div class="container">
+        <div class="section_title">
+            <h2>Features</h2>
+            <p>Check out our best features!!!</p>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 about_des">
+                <img src="images/fleet_pic2.jpg" alt="">
+            </div>
+            <div class="col-lg-6">
+            <p class="mt-3">Ride-sharing services offer comfortable, affordable, and convenient travel options with features like clean, spacious vehicles, real-time tracking, and trained drivers ensuring safety. Passengers can choose from various ride types, enjoy eco-friendly shared rides, and benefit from user-friendly booking systems, multiple payment options, and 24/7 availability. With robust safety measures and customizable preferences, ride-sharing provides a reliable and relaxing travel experience for all.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Features Part Ends-->
+
+<!-- Fleet Part Starts-->
+
+<div id="fleet">
+    <div class="container">
+        <div class="section_title">
+            <h2>Fleet</h2>
+            <p>Watch our fleet which are always determined to provide a safe and sound journey</p>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="column">
+                        <img src="images/fleet_pic1.jpg" style="height: 250px;">
+                        <img src="images/fleet_pic2.jpg" style="height: 350px;">
+                    </div>
+                    <div class="column">
+                        <img src="images/fleet_pic3.jpg" style="height: 350px;">
+                        <img src="images/fleet_pic4.jpg" style="height: 250px;">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <h3 class="text-center" style="font-size: 30px; margin-top: 100px; margin-bottom: 20px;">Comfortable
+                    Rides</h3>
+                <p class="text-center" style="font-size: 18px; margin-bottom: 40px;">Comfortable rides in ridesharing prioritize passenger convenience and satisfaction. Features include well-maintained vehicles, spacious seating, climate control, and professional drivers ensuring a smooth journey. Customizable ride options, such as selecting vehicle types or shared versus private rides, cater to individual preferences, making travel more enjoyable and hassle-free.</p>
+                <div class="counter_container">
+                    <div class="counter col_fourth">
+                        <h2 class="timer count-title count-number" data-to="39137" data-speed="1500"></h2>
+                        <p class="count-text ">Taxis</p>
+                    </div>
+                    <div class="counter col_fourth">
+                        <h2 class="timer count-title count-number" data-to="31376913" data-speed="1500"></h2>
+                        <p class="count-text ">Users</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Fleet Part Ends-->
+
+
+<?php include_once "footer.php" ?>
